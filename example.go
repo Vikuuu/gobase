e1b906d3a831c4907ca36920e522a7d10af2abbd
Vikuuu <adivik672@gmail.com>
2024-11-06 21:40:13 +0530

    chore: Expected outcome in v0.0.1

---
+++ example.go
@@ -0,0 +1,31 @@
+/*
+* This file defines the input and output
+* expected in the gobase v0.0.1
+ */
+package main
+
+import (
+	"time"
+)
+
+// Input struct
+type users struct {
+	ID        int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Email     string
+	IsMember  bool
+	NewField  string
+}
+
+// Output SQL file
+
+/*
+* CREATE TABLE users (
+* 	id INT,
+* 	created_at TIMESTAMP,
+* 	updated_at TIMESTAMP,
+* 	name TEXT
+* );
+ */
