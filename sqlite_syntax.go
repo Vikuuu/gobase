d6cbae1b9ea89142bc183863455b14a5eb5639cc
Vikuuu <adivik672@gmail.com>
2024-12-24 18:30:03 +0530

    feat(migration): Create Mig from changes
    - Added the functionality of adding migration syntax from the changes
    detected.
    - Added unit tests to check the functionality.

--- sqlite_syntax.go
+++ sqlite_syntax.go
@@ -2,9 +2,10 @@ package gobase
 
 import (
 	"fmt"
+	"strings"
 )
 
-func SqLiteCreateTable(fileName string, schema Schema) (cQuery string, dQuery string) {
+func SqLiteCreateTable(schema Schema) (upQuery string, downQuery string) {
 	createQuery := fmt.Sprintf("CREATE TABLE %s (\n\t", schema.SchemaName)
 	for i, field := range schema.SchemaFields {
 		if i == len(schema.SchemaFields)-1 {
@@ -22,4 +23,79 @@ func SqLiteCreateTable(fileName string, schema Schema) (cQuery string, dQuery st
 	dropQuery := fmt.Sprintf("DROP TABLE %s;", schema.SchemaName)
 
 	return createQuery, dropQuery
+}
+
+func SqliteMigration(changes ChangeLog) (upQuery string, downQuery string) {
+	uQuery, dQuery := "", ""
+
+	for _, cChange := range changes.Creations {
+		switch cChange.CreationType {
+		case FIELD:
+			cData := strings.Split(cChange.CreationData, ":")
+			uQuery += fmt.Sprintf(
+				"ALTER TABLE %s\nADD COLUMN %s %s;\n",
+				cChange.TableName,
+				cData[0],
+				sqliteMapping[cData[1]],
+			)
+			dQuery += fmt.Sprintf(
+				"ALTER TABLE %s\nDROP COLUMN %s;\n",
+				cChange.TableName,
+				cData[0],
+			)
+		}
+	}
+
+	for _, dChange := range changes.Deletions {
+		switch dChange.DeletionType {
+		case FIELD:
+			dData := strings.Split(dChange.DeletionData, ":")
+			uQuery += fmt.Sprintf(
+				"ALTER TABLE %s\nDROP COLUMN %s;\n",
+				dChange.TableName,
+				dData[0],
+			)
+			dQuery += fmt.Sprintf(
+				"ALTER TABLE %s\nADD COLUMN %s %s;\n",
+				dChange.TableName,
+				dData[0],
+				sqliteMapping[dData[1]],
+			)
+		}
+	}
+
+	for _, uChange := range changes.Updates {
+		switch uChange.UpdateType {
+		case NAMEUPDATE:
+			switch uChange.ON {
+			case ONTABLE:
+				uQuery += fmt.Sprintf(
+					"ALTER TABLE %s\nRENAME TO %s;\n",
+					uChange.TableName,
+					uChange.UpdateData,
+				)
+				dQuery += fmt.Sprintf(
+					"ALTER TABLE %s\nRENAME TO %s;\n",
+					uChange.UpdateData,
+					uChange.TableName,
+				)
+			case ONFIELD:
+				colNameArr := strings.Split(uChange.UpdateData, ":")
+				uQuery += fmt.Sprintf(
+					"ALTER TABLE %s\nRENAME COLUMN %s to %s;\n",
+					uChange.TableName,
+					colNameArr[0],
+					colNameArr[1],
+				)
+				dQuery += fmt.Sprintf(
+					"ALTER TABLE %s\nRENAME COLUMN %s to %s;\n",
+					uChange.TableName,
+					colNameArr[1],
+					colNameArr[0],
+				)
+			}
+		}
+	}
+
+	return uQuery, dQuery
 }
--- sqlite_syntax_test.go
+++ sqlite_syntax_test.go
@@ -10,12 +10,100 @@ func TestSqLiteCreateTable(t *testing.T) {
 	expectedDownQuery := "DROP TABLE users;"
 	schema := Parse(fileName)
 
-	outputUpQuery, outputDownQuery := SqLiteCreateTable(fileName, schema)
+	outputUpQuery, outputDownQuery := SqLiteCreateTable(schema)
 
 	if expectedUpQuery != outputUpQuery {
 		t.Errorf("Up query err. expected=%s. got=%s", expectedUpQuery, outputUpQuery)
 	}
 	if expectedDownQuery != outputDownQuery {
 		t.Errorf("Down query err. expected=%s. got=%s", expectedDownQuery, outputDownQuery)
+	}
+}
+
+func TestSqliteMigration(t *testing.T) {
+	tests := []struct {
+		name    string
+		change  ChangeLog
+		expUp   string
+		expDown string
+	}{
+		{
+			name:    "No Change",
+			change:  ChangeLog{},
+			expUp:   "",
+			expDown: "",
+		},
+		{
+			name: "Field Creation",
+			change: ChangeLog{
+				Creations: []Create{
+					{
+						CreationType: FIELD,
+						ON:           ONTABLE,
+						TableName:    "users",
+						CreationData: "id:int",
+					},
+				},
+			},
+			expUp:   "ALTER TABLE users\nADD COLUMN id INTEGER;\n",
+			expDown: "ALTER TABLE users\nDROP COLUMN id;\n",
+		},
+		{
+			name: "Field Deletion",
+			change: ChangeLog{
+				Deletions: []Delete{
+					{
+						DeletionType: FIELD,
+						ON:           ONTABLE,
+						TableName:    "users",
+						DeletionData: "id:int",
+					},
+				},
+			},
+			expUp:   "ALTER TABLE users\nDROP COLUMN id;\n",
+			expDown: "ALTER TABLE users\nADD COLUMN id INTEGER;\n",
+		},
+		{
+			name: "Table Rename",
+			change: ChangeLog{
+				Updates: []Update{
+					{
+						UpdateType: NAMEUPDATE,
+						ON:         ONTABLE,
+						TableName:  "users",
+						UpdateData: "accounts",
+					},
+				},
+			},
+			expUp:   "ALTER TABLE users\nRENAME TO accounts;\n",
+			expDown: "ALTER TABLE accounts\nRENAME TO users;\n",
+		},
+		{
+			name: "Field Rename",
+			change: ChangeLog{
+				Updates: []Update{
+					{
+						UpdateType: NAMEUPDATE,
+						ON:         ONFIELD,
+						TableName:  "users",
+						UpdateData: "id:user_id",
+					},
+				},
+			},
+			expUp:   "ALTER TABLE users\nRENAME COLUMN id to user_id;\n",
+			expDown: "ALTER TABLE users\nRENAME COLUMN user_id to id;\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotUp, gotDown := SqliteMigration(tt.change)
+			if gotUp != tt.expUp {
+				t.Errorf("Up Mig Not Same\nExpected: %s\nGot: %s", tt.expUp, gotUp)
+			}
+			if gotDown != tt.expDown {
+				t.Errorf("Down Mig Not Same\nExpected: %s\nGot: %s", tt.expDown, gotDown)
+			}
+		})
 	}
 }
